90215bd5ec75bcaaa1a6e23ab4c11dffd0f332f6
vitor <vitor@localhost.localdomain>
2013-04-26 22:49:51 -0400

    Statement for PgSQL

--- vsql_connection_pgsql.h
+++ vsql_connection_pgsql.h
@@ -11,9 +11,14 @@
 #include <cstdlib>
 #include <sstream>
 #include <fstream>
+#include <map>
+#include <vector>
 #include <libpq-fe.h>
 
 namespace VSQL_PGSQL {
+
+    typedef std::vector<std::string> Row;
+    typedef std::map<std::string, Row> ResultSet;
 
     class Connection {
     private:
@@ -29,6 +34,20 @@ namespace VSQL_PGSQL {
         bool closeConnection();
         std::string showServerVersion();
 
+    };
+
+    class Statement {
+    private:
+        std::string _queryString;
+    public:
+        void bindParam(int param, void * value, int data_type);
+        void bindValue(std::string param, void * value, int data_type);
+        bool execute();
+        Row fetch();
+        ResultSet fetchAll();
+        void * fetchObject();
+        int rowCount();
+        int columnCount();
     };
 }
 
