db35db7f6367f827ea8fe8cd78b2b00a551985d1
vitor <vitor@localhost.localdomain>
2013-05-01 21:13:26 -0300

    Começo da Implementação do MySQL

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,8 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/vsql_mysql.o \
-	${OBJECTDIR}/vsql_pgsql.o \
+	${OBJECTDIR}/vsql_mysql_connection.o \
+	${OBJECTDIR}/vsql_pgsql_connection.o \
 	${OBJECTDIR}/vsql_pgsql_statement.o
 
 
@@ -64,15 +64,15 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libVSQL.${CND_DLIB_EXT}: ${OBJECTFILE
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libVSQL.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
 
-${OBJECTDIR}/vsql_mysql.o: vsql_mysql.cpp 
+${OBJECTDIR}/vsql_mysql_connection.o: vsql_mysql_connection.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -lpq -O -I/usr/include/mysql -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -fno-strict-aliasing -fwrapv -fPIC -fPIC -g -static-libgcc -fno-omit-frame-pointer -fno-strict-aliasing -DMY_PTHREAD_FASTMUTEX=1 -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lrt -lssl -lcrypto -ldl -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/vsql_mysql.o vsql_mysql.cpp
+	$(COMPILE.cc) -g -lpq -O -I/usr/include/mysql -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -fno-strict-aliasing -fwrapv -fPIC -fPIC -g -static-libgcc -fno-omit-frame-pointer -fno-strict-aliasing -DMY_PTHREAD_FASTMUTEX=1 -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lrt -lssl -lcrypto -ldl -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/vsql_mysql_connection.o vsql_mysql_connection.cpp
 
-${OBJECTDIR}/vsql_pgsql.o: vsql_pgsql.cpp 
+${OBJECTDIR}/vsql_pgsql_connection.o: vsql_pgsql_connection.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -g -lpq -O -I/usr/include/mysql -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -fno-strict-aliasing -fwrapv -fPIC -fPIC -g -static-libgcc -fno-omit-frame-pointer -fno-strict-aliasing -DMY_PTHREAD_FASTMUTEX=1 -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lrt -lssl -lcrypto -ldl -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/vsql_pgsql.o vsql_pgsql.cpp
+	$(COMPILE.cc) -g -lpq -O -I/usr/include/mysql -g -pipe -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -fno-strict-aliasing -fwrapv -fPIC -fPIC -g -static-libgcc -fno-omit-frame-pointer -fno-strict-aliasing -DMY_PTHREAD_FASTMUTEX=1 -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lrt -lssl -lcrypto -ldl -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/vsql_pgsql_connection.o vsql_pgsql_connection.cpp
 
 ${OBJECTDIR}/vsql_pgsql_statement.o: vsql_pgsql_statement.cpp 
 	${MKDIR} -p ${OBJECTDIR}
--- vsql_mysql.h
+++ vsql_mysql.h
@@ -20,36 +20,59 @@ namespace VSQL_MYSQL {
     typedef std::vector<std::string> Row;
     typedef std::map<std::string, Row> ResultSet;
 
-    class Connection {
-    private:
-        std::string host;
-        std::string user;
-        std::string passwd;
-        std::string dbname;
-        bool in_transaction;
-
-    public:
-        Connection(std::string str_connection);
-        bool openConnection();
-        bool closeConnection();
-        std::string showServerVersion();
-
-    };
-
-    class Statement {
+     class Statement {
     private:
         std::string _queryString;
+        MYSQL * _conn;
+        MYSQL_RES * _result_set;
+        MYSQL_ROW _mysql_row;
+        Row _row;
+        ResultSet _result;
+        int _total_rows;
+        int _total_cols;
+        void clearResultSet();
     public:
-        void bindParam(int param, void * value, int data_type);
+        Statement(std::string sql, MYSQL * conn);
+        void bindParam(void * value, int data_type);
         void bindValue(std::string param, void * value, int data_type);
         bool execute();
         Row fetch();
         ResultSet fetchAll();
-        void * fetchObject();
         int rowCount();
         int columnCount();
     };
+
+    class Connection {
+    private:
+        std::string _host;
+        std::string _user;
+        std::string _passwd;
+        std::string _dbname;
+        int _port;
+        bool _in_transaction;
+        MYSQL * _conn;
+        MYSQL_RES * _result_set;
+        MYSQL_ROW _mysql_row;
+        std::string _error_message;
+        void clearResultSet();
+
+    public:
+        Connection(std::string host, std::string user, std::string passwd, std::string dbname, int port);
+        bool openConnection();
+        bool beginTransaction();
+        bool savePointTransaction(std::string savePointName);
+        bool commitTransaction();
+        bool rollbackTransaction();
+        bool exec(std::string sql);
+        VSQL_MYSQL::Statement * prepare(std::string sql);
+        VSQL_MYSQL::Statement * query(std::string sql);
+        bool closeConnection();
+        std::string getErrorMessage();
+        std::string getServerVersion();
+    };
 }
+
+
 //PGSQL Version "show server_version"
 //MYSQL Version "select VERSION();"
 #endif	/* CONNECTION_H */
