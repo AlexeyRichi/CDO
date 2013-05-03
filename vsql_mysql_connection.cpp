5cf890265d6a6be61802f2c0d9475677fabe3179
vitor <vitor@localhost.localdomain>
2013-05-02 21:37:10 -0300

    Implementação das funções de Transaction

--- vsql_mysql_connection.cpp
+++ vsql_mysql_connection.cpp
@@ -13,12 +13,82 @@ VSQL_MYSQL::Connection::Connection(std::string host, std::string user, std::stri
 }
 
 bool VSQL_MYSQL::Connection::openConnection() {
+    //Inicia o ponteiro
     this->_conn = mysql_init(NULL);
+
+    //Tenta Conectar
     if (!mysql_real_connect(this->_conn, this->_host.c_str(), this->_user.c_str(), this->_passwd.c_str(), this->_dbname.c_str(), this->_port, NULL, 0)) {
+        //Se falhar, grava a mensagem de erro
         this->_error_message.append(mysql_error(this->_conn));
         return false;
     } else {
         return true;
     }
 
+}
+
+bool VSQL_MYSQL::Connection::beginTransaction() {
+    //Tentando começar a transaction
+
+    //Desliga o AutoCommit
+    if (!mysql_autocommit(this->_conn, false)) {
+        this->_error_message.clear();
+        this->_error_message.append(mysql_error(this->_conn));
+        return false;
+    }
+
+    if (mysql_query(this->_conn, "BEGIN;")) {
+        this->_error_message.clear();
+        this->_error_message.append(mysql_error(this->_conn));
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+bool VSQL_MYSQL::Connection::savePointTransaction(std::string savePointName) {
+    char * sql = new char[200];
+    sprintf(sql, "SAVEPOINT %s", savePointName.c_str());
+    if (mysql_query(this->_conn, sql)) {
+        this->_error_message.clear();
+        this->_error_message.append(mysql_error(this->_conn));
+        delete(sql);
+        return false;
+    } else {
+        delete(sql);
+        return true;
+    }
+}
+
+bool VSQL_MYSQL::Connection::commitTransaction() {
+        if (mysql_query(this->_conn, "COMMIT;")) {
+        this->_error_message.clear();
+        this->_error_message.append(mysql_error(this->_conn));
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+bool VSQL_MYSQL::Connection::rollbackTransaction() {
+    
+    if (mysql_query(this->_conn, "ROLLBACK;")) {
+        this->_error_message.clear();
+        this->_error_message.append(mysql_error(this->_conn));
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+bool VSQL_MYSQL::Connection::exec(std::string sql) {
+
+}
+
+bool VSQL_MYSQL::Connection::closeConnection() {
+    mysql_close(this->_conn);
+    return true;
 }
